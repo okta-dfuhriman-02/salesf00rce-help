aa72f2b4b0f255b03f5a51864c33ddf5d52452af
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-05-09 18:59:04 -0700

    rename page

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,7 +5,7 @@ import AppLoginCallback from './pages/LoginCallback';
 import HelpHeader from './components/HelpHeader';
 import PageSpinner from './components/PageSpinner';
 import SecureApp from './components/SecureApp';
-import HelpPage from './pages/Today';
+import HelpPage from './pages/Help';
 
 const Router = () => {
 	const { isLoading } = Auth.useAuthState();

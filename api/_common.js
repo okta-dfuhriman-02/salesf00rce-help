eba40162b23dd2527d142d0f15a585fa339944a6
Danny Fuhriman <eatplaysleep@gmail.com>
2022-05-09 19:07:08 -0700

    Develop > Master (#8)
    * rename page
    
    * remove dead code

--- api/_common.js
+++ api/_common.js
@@ -6,5 +6,4 @@ export { default as getLinkedProfiles } from './_getLinkedProfiles';
 export { default as getCredentials } from './_getCredentials';
 export { default as getOktaUser } from './_getOktaUser';
 export { default as getUnifiedProfile } from './_getUnifiedProfile';
-export { default as updateUnifiedProfile } from './_updateUser';
 export { default as JwtVerifier } from './_customJwtVerifier';
